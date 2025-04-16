8c35b07d2b755721f712e522366dfd74e778ec29
Ted-DevWeb <109811585+TedPasquiet@users.noreply.github.com>
2025-04-16 22:30:23 +0200

    path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/portfolio3D/" : "",
+  base: '/portfolio3D/',
   plugins: [react(), tailwindcss()],
 });
